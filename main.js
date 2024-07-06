f285e4a3b5f5444d1df9655d6c9823693a09ac1d
vanessatan1509 <vanessatan1509@gmail.com>
2024-07-07 00:35:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,97 @@
+/* Toggle Icon Navbar */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+/* Scroll Section Active Link */
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    /* Sticky Navbar */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* Remove Toggle Icon and Navbar */
+    let menuIcon = document.querySelector('#menu-icon'); 
+    let navbar = document.querySelector('.navbar'); 
+    if (menuIcon && navbar) {
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    }
+};
+
+
+/* Scroll Reveal */
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+/* TYPED JS */
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer', 'Youtuber'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
+
+/* About Skills */
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+/* FORM - CONTACT ME - SUBMIT TO GOOGLE SHEET */
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz6KuU7eDhQBW7eKnvSTgjE5PwMhGO2n9BSvSgvdwLYzCjhRxs1CKsLcsoN-Pc0LQd5EA/exec'
+const form = document.forms['submit-to-google-sheet']
+const mgs = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        mgs.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },3000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
